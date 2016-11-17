4b02e6025f9855f24d8912e06fa23865c4e9fc53
TJ Boskelly <tjboskelly@gmail.com>
2016-11-17 15:21:42 -0600

    Add files via upload

--- carousel.js
+++ carousel.js
@@ -1,25 +1,21 @@
-$(document).ready(function() {  
-    $('#carousel_ul li:first').before($('#carousel_ul li:last'));  
-       $('#right_scroll img').click(function(){  
-           var item_width = $('#carousel_ul li').outerWidth() + 10;  
-           var left_indent = parseInt($('#carousel_ul').css('left')) - item_width;  
-           $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){  
-               $('#carousel_ul li:last').after($('#carousel_ul li:first'));  
-               $('#carousel_ul').css({'left' : '-210px'});  
-            });  
-        });   
-        $('#left_scroll img').click(function(){  
-  
-            var item_width = $('#carousel_ul li').outerWidth() + 10;  
-
-            var left_indent = parseInt($('#carousel_ul').css('left')) + item_width;  
-  
-            $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){  
-
-            $('#carousel_ul li:first').before($('#carousel_ul li:last'));  
-  
-            $('#carousel_ul').css({'left' : '-210px'});  
-            });  
-  
-        });  
-  });  
+var carouselApp = angular.module('carouselApp',[]);
+      carouselApp.controller('carouselCtrl', function($scope) {
+         $scope.links =[
+           { src:"../carouselimages/image1.jpeg", alt:"", caption:""},
+        ];
+      });
+      carouselApp.directive('carousel', function($timeout) {
+         return {
+            restrict: 'E',
+            scope: {
+              links: '='
+            },
+            templateUrl: 'carousel.html',
+            link: function(scope, element) {
+              $timeout(function() {
+                $('.carousel-indicators li',element).first().addClass('active');
+                $('.carousel-inner .item',element).first().addClass('active');
+              });
+            }
+         }
+      });
--- crafts.js
+++ crafts.js
@@ -1,5 +1,5 @@
 (function(){
-    var app = angular.module('store', [ ]);
+    var app = angular.module('store', []);
     
     app.controller('StoreController', function(){
         this.product = craftbeers;
@@ -7,169 +7,155 @@
     
     var craftbeers = [
         {
-        name: 'Craft Beer 1',
-        brewery: 'Brewery 1',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Christmas Ale',
+        brewery: 'Great Lakes Brewing Company',
+        description: 'A Yuletide’s worth of holiday spices and sweet honey to keep you a-wassailing all season long.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image1.jpeg',
             }
         ]
         },
         {
-        name: 'Craft Beer 2',
-        brewery: 'Brewery 2',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Bells Christmas Ale',
+        brewery: 'Bells Brewery Inc',
+        description: 'A blend of hops from Michigan and the Pacific Northwest lend an earthy, herbal aroma to compliment your holiday menu, not overpower it.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image2.jpeg',
             }
         ]
         },
         {
-        name: 'Craft Beer 3',
-        brewery: 'Brewery 3',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Christmas Ale',
+        brewery: 'Breckenridge Brewery',
+        description: 'A sturdy texture and rich flavors of caramel and chocolate, our holiday seasonal is the fermented equivalent of a good fire.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image3.jpeg',
             }
         ]
         },
         {
-        name: 'Craft Beer 4',
-        brewery: 'Brewery 4',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: '12 Dogs of Christmas Ale',
+        brewery: 'Thristy Dog Brewing Company',
+        description: 'Filled with seasonal flavors from generous amounts of toasted and caramel malts, mixed with equality generous amounts of honey, cinnamon, ginger, and nutmeg.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image4.jpeg',
             }
         ]        
         },
         {
-        name: 'Craft Beer 5',
-        brewery: 'Brewery 5',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Saint Arnold Christmas Ale',
+        brewery: 'Saint Arnold Brewing Company',
+        description: 'A rich, copper colored, hearty ale perfect for the holiday season with a malty sweetness and spicy hop character.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image5.jpeg',
             }
         ]        
         },
         {
-        name: 'Craft Beer 6',
-        brewery: 'Brewery 6',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Schlafly Christmas Ale',
+        brewery: 'The Schlafly Tap Room',
+        description: 'Warming winter ale that blends the spices of the season with sweet caramel malt.  Orange peel, juniper berries, ginger root, cardamom and cloves are added for spiciness.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image6.jpeg',
             }
         ]        
         },
         {
-        name: 'Craft Beer 7',
-        brewery: 'Brewery 7',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Frosted Frog Christmas Ale',
+        brewery: 'Hoppin Frog Brewery',
+        description: 'Perfectly blended spices compliment Frosted Frog’s rich malt flavors, creating the ultimate Christmas experience.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image7.jpeg',
             }
         ]            
         },
         {
-        name: 'Craft Beer 8',
-        brewery: 'Brewery 8',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Christmas BOMB!',
+        brewery: 'Prairie Artisan Ales',
+        description: 'Imperial stout aged on espresso beans, chocolate, vanilla beans, and ancho chile peppers & Christmas spices.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image8.jpeg',
             }
         ]            
         },
         {
-        name: 'Craft Beer 9',
-        brewery: 'Brewery 9',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Christmas Ale',
+        brewery: 'Upslope Brewery',
+        description: 'Holiday spices complement the dark fruit and spicy notes from Abbey Ale Yeast. The distinctive holiday aroma, ruby red color and rich malt flavor make this ale perfect for spreading Christmas cheer.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image9.jpeg',
             }
         ]            
         },
         {
-        name: 'Craft Beer 10',
-        brewery: 'Brewery 10',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Christmas Ale',
+        brewery: 'West Sixth Brewing',
+        description: 'Spiced ale with aroma of cinnamon and nutmeg with a sturdy malt backbone that will hold up to the chilliest of nights.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image10.jpeg',
             }
         ]            
         },
         {
-        name: 'Craft Beer 11',
-        brewery: 'Brewery 11',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Frankenmuth Christmas Town Ale',
+        brewery: 'Frankenmuth Brewery',
+        description: 'Combines sweet toasted dark malts with American hops and subtle notes of nutmeg, ginger, cinnamon and clove.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image11.jpeg',
             }
         ]            
         },
         {
-        name: 'Craft Beer 12',
-        brewery: 'Brewery 12',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Jubelale',
+        brewery: 'Deschutes Brewery',
+        description: 'Cocoa, dried fruit, and toffee notes. A robust ale with a warming spice.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image12.jpeg',
             }
         ]            
         },
         {
-        name: 'Craft Beer 13',
-        brewery: 'Brewery 13',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Christmas Ale',
+        brewery: 'Anchor Brewing',
+        description: 'Deep mahogany brown with a creamy, tan head and boasts aromas of fruitcake, molasses, and fresh cut wood. The beer tastes of a roasted caramel malt, with notes of spiced chocolate and nuts.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image13.jpeg',
             }
         ]            
         },
          {
-        name: 'Craft Beer 14',
-        brewery: 'Brewery 14',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Milk Stout Nitro',
+        brewery: 'Left Hand Brewing Company ',
+        description: 'The pillowy head coats your upper lip and its creaminess entices your palate. Initial roasty, mocha flavors rise up, with slight hop & roast bitterness in the finish.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image14.jpeg',
             }
         ]            
         },
          {
-        name: 'Craft Beer 15',
-        brewery: 'Brewery 15',
-        price: 0.00,
-        description: 'blah, blah, blah',
+        name: 'Winter Warlock ',
+        brewery: 'Bristol Brewing Company',
+        description: 'Chock full of highly roasted barley malt and 150 lbs. of flaked oatmeal, the Warlock is a dark and satisfying winter elixir. Smooth, rich and exceptionally comforting.',
         images:[
             {
-                full: 'img1.png',
+                full: '../craftsimages/image15.jpeg',
             }
         ]            
         },
         ];
+    
 })();
